1744239ed76ecb61a769605c84a2b50bf237f6b0
Mar'i Fauzan <marifauzan20@gmail.com>
2021-03-10 17:49:09 +0700

    Membuat Get Previous Hash dan Update atribut Blockchain

--- app.js
+++ app.js
@@ -4,21 +4,14 @@ const app = express();
 const pool = require("./db");
 // const SHA256 = require('crypto-js/sha256');
 // const Block = require("./blockchain");
-const { ClientBase } = require("pg");
 // var router = require("./app/route/route");
 const port = 3000;
 
 // app.use("/api/v1/node", router);
 
-app.use(express.json()); // req.body
+app.use(express.json()); // REQ.BODY
 
-// get all voter
-
-// () => {
-//
-
-// }
-
+// GET ALL VOTER
 app.get("/voter", async (req, res) => {
   try {
     const allVoter = await pool.query("SELECT * FROM blockvoter;");
@@ -28,40 +21,28 @@ app.get("/voter", async (req, res) => {
     console.log(err.message);
   }
 });
+// GET ALL VOTER
 
-//create a voter
+// CREATE A VOTER
 app.post("/voter", async (req, res) => {
   try {
-    const { index } = req.body;
-    const { name } = req.body;
-    const { email } = req.body;
-    const { polling_booth } = req.body;
-    const { admin_name } = req.body;
-    const { previous_hash } = req.body;
-
-    let Voter = new Block(
-      index,
-      name,
-      email,
-      polling_booth,
-      admin_name,
-      previous_hash
-    );
-
-    // get prev hash
-    // Insert Genesis Block
+    // INSERT GENESIS BLOCK
     const genesis = await pool.query("SELECT * FROM blockvoter;");
+
     if (genesis.rows == 0) {
       let genesisBlock = new Block(
-        0,
-        "genesis",
-        "email@Genesis.com",
-        0,
-        "genAdmin",
-        "iniceritanyahashpanjanggitu"
+        "Genesis Block",
+        "Genesis Block",
+        "Genesis Block",
+        "Genesis Block",
+        "Genesis Block",
+        "Genesis Block",
+        "Genesis Block",
+        "Genesis Block"
       );
+
       const genesisInsert = await pool.query(
-        "INSERT INTO blockvoter (index,name,email,admin_name,polling_booth,previous_hash) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;",
+        "INSERT INTO blockvoter (index,name,email,polling_booth,admin_name,timestamp,hash,previous_hash) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *;",
         // [genesisBlock.calculateProperty(genesisBlock.index),genesisBlock.calculateProperty(genesisBlock.name), genesisBlock.calculateProperty(genesisBlock.email),genesisBlock.calculateProperty(genesisBlock.polling_booth),
         //   genesisBlock.calculateProperty(genesisBlock.admin_name),genesisBlock.calculateProperty(genesisBlock.previous_hash)]
         [
@@ -70,14 +51,41 @@ app.post("/voter", async (req, res) => {
           genesisBlock.email,
           genesisBlock.polling_booth,
           genesisBlock.admin_name,
+          genesisBlock.timestamp,
+          genesisBlock.hash,
           genesisBlock.previous_hash,
         ]
       );
-      res.json(genesisInsert.rows[0]);
     }
-    // create new hash
+    // END INSERT GENESIS BLOCK
+
+    // GET PREVIOUS HASH
+    const previousHash = await pool.query("SELECT hash FROM blockvoter;");
+    // END GET PREVIOUS HASH
+
+    // CREATE NEW HASH
+    const { index } = req.body;
+    const { name } = req.body;
+    const { email } = req.body;
+    const { polling_booth } = req.body;
+    const { admin_name } = req.body;
+    const { timestamp } = req.body;
+    const { hash } = req.body;
+    const previous_hash = previousHash.rows[previousHash.rows.length - 1].hash;
+
+    let Voter = new Block(
+      index,
+      name,
+      email,
+      polling_booth,
+      admin_name,
+      timestamp,
+      hash,
+      previous_hash
+    );
+
     const newVoter = await pool.query(
-      "INSERT INTO blockvoter (index,name,email,admin_name,polling_booth,previous_hash) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;",
+      "INSERT INTO blockvoter (index,name,email,polling_booth,admin_name,timestamp,hash,previous_hash) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *;",
       // [Voter.calculateProperty(Voter.index),Voter.calculateProperty(Voter.name), Voter.calculateProperty(Voter.email),Voter.calculateProperty(Voter.polling_booth),
       //   Voter.calculateProperty(Voter.admin_name),Voter.calculateProperty(Voter.previous_hash)]
       [
@@ -86,6 +94,8 @@ app.post("/voter", async (req, res) => {
         Voter.email,
         Voter.polling_booth,
         Voter.admin_name,
+        Voter.timestamp,
+        Voter.hash,
         Voter.previous_hash,
       ]
     );
@@ -96,6 +106,7 @@ app.post("/voter", async (req, res) => {
     console.error(err.message);
   }
 });
+// END CREATE A VOTER
 
 app.listen(port, () => {
   console.log(`Node app is live at port ${port}`);
--- blockchain.js
+++ blockchain.js
@@ -1,20 +1,29 @@
 const SHA256 = require("crypto-js/sha256");
-
-// console.log(SHA256("abaaa").toString()); percobaan
-
 class Block {
-  constructor(index, name, email, polling_booth, admin_name, previous_hash) {
+  constructor(
+    index,
+    name,
+    email,
+    polling_booth,
+    admin_name,
+    timestamp,
+    hash,
+    previous_hash
+  ) {
     this.index = index;
     this.name = name;
     this.email = email;
     this.polling_booth = polling_booth;
     this.admin_name = admin_name;
+    this.timestamp = timestamp;
+    this.hash = hash;
     this.previous_hash = previous_hash;
   }
 
   calculateProperty(property) {
     return SHA256(property).toString();
   }
+
   calculateHash() {
     return SHA256(
       this.calculateProperty(this.index) +
@@ -22,31 +31,12 @@ class Block {
         this.calculateProperty(this.email) +
         this.calculateProperty(this.polling_booth) +
         this.calculateProperty(this.admin_name) +
+        this.calculateProperty(this.timestamp) +
+        this.calculateProperty(this.hash) +
         this.calculateProperty(this.previous_hash)
     ).toString();
   }
   // isValid()
 }
 
-const Genesis = new Block(
-  0,
-  "Genesis Block",
-  "genesis@email.com",
-  99,
-  "Genesis"
-);
-
-const Daffa = new Block(
-  1,
-  "daffa",
-  "barin@email.com",
-  4,
-  "dinu",
-  Genesis.calculateHash()
-);
-
-console.log(Daffa.previous_hash);
-
 module.exports = Block;
-// console.log(Daffa.calculateHash());
-// console.log(Genesis.calculateHash());
